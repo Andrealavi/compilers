5a9d1fb5967a98c236e039b6fe52db1aa00e53a6
Andrea Lavino <andrealavino1410.2003@gmail.com>
2025-02-03 17:56:36 +0100

    Created first file for the recursive descent parser

---
+++ recursive_descent_parser/parser.py
@@ -0,0 +1,25 @@
+class Parser:
+    def __init__(self, grammar: dict):
+        self.grammar = grammar
+        self.parsing_table = {}
+
+    def parse(self, input_str: str) -> bool:
+        return True
+
+def get_terminals_from_grammar(grammar: dict) -> list[str]:
+    return []
+
+def read_grammar(filename = "") -> dict[str, list[str]]:
+    return {}
+
+def first(phrase: str, grammar: dict) -> list[str]:
+    return []
+
+def follow(nonterminal: str, grammar: dict) -> list[str]:
+    return []
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
