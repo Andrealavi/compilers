5ca16029d0213a318e20bd5f390ffbcaf31dcc0e
Andrea Lavino <andrealavino1410.2003@gmail.com>
2025-02-04 10:27:18 +0100

    Implemented get_terminals_from_grammar and first functions

--- recursive_descent_parser/parser.py
+++ recursive_descent_parser/parser.py
@@ -7,7 +7,15 @@ class Parser:
         return True
 
 def get_terminals_from_grammar(grammar: dict) -> list[str]:
-    return []
+    terminals: list[str] = []
+
+    for tails in grammar.values():
+        for tail in tails:
+            for token in tail:
+                if token not in grammar.keys():
+                    terminals.append(token)
+
+    return terminals
 
 def read_grammar(filename) -> dict[str, list[str]]:
     grammar: dict[str, list[str]] = {}
@@ -24,14 +32,53 @@ def read_grammar(filename) -> dict[str, list[str]]:
 
     return grammar
 
-def first(phrase: str, grammar: dict) -> list[str]:
-    return []
+def first(phrase: str, grammar: dict) -> set[str]:
+    first_set: set[str] = set()
+
+    queue = []
+
+    i = 0
+    phrase_len = len(phrase)
+    char = phrase[i]
+
+    while i < phrase_len:
+        if char in grammar.keys():
+            [queue.insert(0, tail) for tail in grammar[char] if tail not in queue]
+
+            if "ɛ" not in grammar[char]:
+                break
+        else:
+            first_set.add(char)
+            break
+
+        i += 1
+
+    if (i == phrase_len):
+        first_set.add("ɛ")
+
+    while queue:
+        tail = queue.pop()
+
+        for char in tail:
+            if char in grammar.keys():
+                [queue.insert(0, tail) for tail in grammar[char] if tail not in queue and tail[0] != char]
+
+                if "ɛ" not in grammar[char]:
+                    break
+            else:
+                first_set.add(char)
+                break
+
+    return first_set
+
 
 def follow(nonterminal: str, grammar: dict) -> list[str]:
     return []
 
 def main():
-    print(read_grammar("prova.txt"))
+    grammar = read_grammar("./prova2.txt")
+
+    print(first("T", grammar))
 
 if __name__ == "__main__":
     main()
